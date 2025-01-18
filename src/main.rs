6e0c0d2831b999b0247041284661041a01b9e9d4
Tomasz Kawalec <tkawalec369@gmail.com>
2025-01-19 00:35:23 +0100

    Add extract command - it works for plaintext, but not yet for encrypted messages, and deosn't verify signatures yet

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,15 @@ enum Commands {
         sign: Option<String>,
         file: String,
     },
+    Extract {
+        #[arg(short = 'k', long = "private-key")]
+        private_key: Option<String>,
+        #[arg(short = 'v', long = "verify")]
+        verify_key: Option<String>,
+        #[arg(short = 'o', long = "output")]
+        output: Option<String>,
+        refe: String,
+    },
 }
 
 async fn ensure_db_dirs(global_args: &GlobalArgs) -> Result<()> {
@@ -74,17 +83,17 @@ async fn main() -> Result<()> {
             if block::block_verify(&args.global_args, &hash).await? {
                 println!("Verification succesful");
 
-                let block = block::read_block(&args.global_args, &hash).await?;
+                let this_block = block::read_block(&args.global_args, &hash).await?;
 
-                if let Some(plain) = block.content.plain {
+                if let Some(plain) = this_block.content.plain {
                     println!("It contains plaintext data");
                 } else {
                     println!("It doesn't contain plaintext data");
                 }
-                if block.content.encrypted.len() == 0 {
+                if this_block.content.encrypted.len() == 0 {
                     println!("It doesn't contain encrypted data");
                 }
-                for enc in block.content.encrypted {
+                for enc in this_block.content.encrypted {
                     // TODO: print public key of rec
                 }
             } else {
@@ -152,6 +161,45 @@ async fn main() -> Result<()> {
 
             let hash = block::write_block(&args.global_args, &block).await?;
             println!("Created new block {}", hash);
+        }
+        Commands::Extract {
+            private_key,
+            verify_key,
+            output,
+            refe,
+        } => {
+            let hash = refs::Ref::from_str(&args.global_args, &refe).await?;
+            if block::block_verify(&args.global_args, &hash).await? {
+                let this_block = block::read_block(&args.global_args, &hash).await?;
+
+                let msg = if let Some(private_key) = private_key {
+                    let private_key_content = tokio::fs::read_to_string(&private_key).await?;
+                    let private_key =
+                        <rsa::RsaPrivateKey as rsa::pkcs1::DecodeRsaPrivateKey>::from_pkcs1_pem(
+                            &private_key_content,
+                        )?;
+                    let msg_encrypted_base64 = &this_block.content.encrypted[0];
+                    let msg_encrypted = base64::engine::general_purpose::STANDARD_NO_PAD
+                        .decode(msg_encrypted_base64)?;
+                    private_key.decrypt(rsa::Pkcs1v15Encrypt, &msg_encrypted)?
+                } else {
+                    // Read plaintext
+                    let msg_base64 = this_block
+                        .content
+                        .plain
+                        .ok_or("Block doesn't contain plaintext message")
+                        .expect("Block doesn't contain plaintext message");
+                    base64::engine::general_purpose::STANDARD_NO_PAD.decode(msg_base64)?
+                };
+
+                if let Some(output) = output {
+                    // TODO
+                } else {
+                    println!("{}", String::from_utf8(msg)?);
+                }
+            } else {
+                eprintln!("Verification failed!");
+            }
         }
     }
 
